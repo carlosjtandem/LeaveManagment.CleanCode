2a1b4ccb9e43f3e7e8b17813e5a265b1e042e927
carlosjtandem <soycarlosjavier14@gmail.com>
2022-11-04 13:46:51 -0500

    Created structure

---
+++ HR.LeaveManagment.Clean.Domain/LeaveAllocation.cs
@@ -0,0 +1,18 @@
+﻿using HR.LeaveManagment.Clean.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Data
+{
+    public class LeaveAllocation: BaseDomainEntity
+    {
+        public int NumberOfDays { get; set; }
+        public LeaveType? LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public int Period { get; set; }
+    }
+}
---
+++ HR.LeaveManagment.Clean.Domain/LeaveType.cs
@@ -0,0 +1,23 @@
+﻿using HR.LeaveManagment.Clean.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Data
+{
+    public class LeaveType: BaseDomainEntity
+    {
+        public string Name { get; set; }
+
+        public LeaveType(string name, int defaultDays)
+        {
+            Name = name;
+            DefaultDays = defaultDays;
+        }
+
+        public int DefaultDays { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
